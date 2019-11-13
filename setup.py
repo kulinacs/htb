80524b3254d07d9bc83785f5d7f8b229c7f346e2
Zach Hanson <zachhanson94@gmail.com>
2019-11-12 18:57:12 -0800

    Incremented version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='htb',
-    version='1.2.0',
+    version='1.2.1',
 
     description='Hack the Box API',
     long_description=long_description,
